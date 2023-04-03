c8b2c518953d6b6d498e9e5e402baf8a9f4bbb96
BunghezOvidiu37 <ovidiueduard@gmail.com>
2023-04-03 20:24:50 +0300

    added the missing import for the button_slide_second

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Button_slide } from "./Atoms/Button_Slide";
 export { default as Button_Line_Drawing } from "./Atoms/Button_Line_Drawing";
+export { default as Button_Slide_Second } from "./Atoms/Button_Slide_Second";
