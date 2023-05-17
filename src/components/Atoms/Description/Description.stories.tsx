ae2c425d438073995e5ef94376f06b3528c7800d
Bunghez Ovidiu <ovidiueduard@gmail.com>
2023-05-17 19:35:29 +0300

    feat(molecule:description): file cleaning + added necessary options

--- src/components/Atoms/Description/Description.stories.tsx
+++ src/components/Atoms/Description/Description.stories.tsx
@@ -5,58 +5,14 @@ import Description from "./Description";
 const Extra_Info = `
 <br>
 <h2><u>PROPS</u></h2>
-<table>
-  <tr>
-    <td>name</td>
-    <td>type</td>
-    <td>can be:</td>
-  </tr>
-  <tr>
-    <td>text</td>
-    <td>string</td>
-    <td>-</td>
-  </tr>
-  <tr>
-    <td>color</td>
-    <td>string</td>
-    <td>hex || color name || rgb</td>
-  </tr>
-  <tr>
-    <td>backgroundColor</td>
-    <td>string</td>
-    <td>hex || color name || rgb</td>
-  </tr>
-</table>
+
 <br>
-<ol>
-  <li>--a-label-color</li>
-  <li>--a-label-background-color</li>
-  <li>--a-label-padding</li>
-  <li>--a-label-border-radius</li>
-</ol>
-<table>
-<tr>
-  <td>--a-label-color</td>
-  <td>$color-white</td>
-</tr>
-<tr>
-  <td>--a-label-background-color</td>
-  <td>$color-black</td>
-</tr>
-<tr>
-  <td>--a-label-padding</td>
-  <td>5px</td>
-</tr>
-<tr>
-  <td>--a-label-border-radius</td>
-  <td>$border-radius-xxl</td>
-</tr>
-</table>
+
 
 `;
 
 export default {
-  title: "Atoms/Label",
+  title: "Atoms/Description",
   component: Description,
   parameters: {
     actions: { disabled: true },
@@ -70,10 +26,11 @@ export default {
 
 const Template: ComponentStory<typeof Description> = (args) => <Description {...args} />;
 
-export const Label_story = Template.bind({});
+export const Description_story = Template.bind({});
 
-Label_story.args = {
-  text: "Placeholder text",
-  color: "white",
-  backgroundColor: "black",
+Description_story.args = {
+  underline: true,
+  italic: true,
+  text: <h1>Vel massa erat deseruisse eripuit aenean <Description text={<h1>cacat</h1>} color="red"/> inimicus sadipscing proin</h1>,
+  color: "black",
 };
--- src/components/Atoms/Description/Description.tsx
+++ src/components/Atoms/Description/Description.tsx
@@ -1,25 +1,24 @@
 import React, { ReactElement, CSSProperties } from "react";
-import "./Label.scss";
+import "./Description.scss";
 
-interface LabelStyle extends CSSProperties {
-  "--a-label-color"?: string;
-  "--a-label-background-color"?: string;
+interface DescriptionStyle extends CSSProperties {
+  "--a-description-color"?: string;
 }
 
 export interface LabelProps {
-  text: string;
-  color: string;
-  backgroundColor: string;
-  style?: LabelStyle;
+  text?: ReactElement;
+  color?: string;
+  style?: DescriptionStyle;
+  underline?: boolean;
+  italic?: boolean;
 }
 
 export default function Description(props: LabelProps): ReactElement {
   return (
     <div
-      className="a-label"
+      className={`a-description ${props.underline ? "underline" : ""} ${props.italic ? "italic" : ""}`}
       style={{
-        "--a-label-color": props.color,
-        "--a-label-background-color": props.backgroundColor,
+        "--a-description-color": props.color,
         ...props.style,
       }}
     >
