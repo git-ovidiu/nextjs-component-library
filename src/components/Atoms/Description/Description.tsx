6e3bfb37860e43b32f1dfc006651f41671787f8b
Bunghez Ovidiu <ovidiueduard@gmail.com>
2023-05-19 09:32:27 +0300

    feat(ato -> animated_text):  solved error for the Description.tsx + release

--- src/components/Atoms/Description/Description.tsx
+++ src/components/Atoms/Description/Description.tsx
@@ -2,28 +2,29 @@ import React, { ReactNode, CSSProperties } from "react";
 import "./Description.scss";
 
 interface DescriptionStyle extends CSSProperties {
-  "--a-description-color"?: string;
+    "--a-description-color"?: string;
 }
 
 export interface DescriptionProps {
-  style?: DescriptionStyle;
-
-  text?: ReactNode | string;
-  color?: string;
-  underline?: boolean;
-  italic?: boolean;
+    style?: DescriptionStyle;
+    text?: ReactNode | string;
+    color?: string;
+    underline?: boolean;
+    italic?: boolean;
 }
 
-export default function Description(props: DescriptionProps): ReactNode {
-  return (
-    <div
-      className={`a-description ${props.underline ? "underline" : ""} ${props.italic ? "italic" : ""}`}
-      style={{
-        "--a-description-color": props.color,
-        ...props.style,
-      }}
-    >
-      {props.text}
-    </div>
-  );
+export default function Description(props: DescriptionProps): JSX.Element {
+    return (
+        <div
+            className={`a-description ${props.underline ? "underline" : ""} ${
+                props.italic ? "italic" : ""
+            }`}
+            style={{
+                "--a-description-color": props.color,
+                ...props.style,
+            }}
+        >
+            {props.text}
+        </div>
+    );
 }
