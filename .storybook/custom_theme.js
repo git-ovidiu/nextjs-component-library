ff448bfcb984730e8fd6f6f6885edab1a7c586d0
Bunghez Ovidiu <ovidiueduard@gmail.com>
2023-05-31 00:25:37 +0300

    fix(master): added .storybook

---
+++ .storybook/custom_theme.js
@@ -0,0 +1,24 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "dark",
+  appBg: "white",
+
+  // appBg: "black",
+  appBorderColor: "#4cbfa6",
+  appBorderRadius: 20,
+
+  textColor: "#4cbfa6",
+  colorPrimary: "#4cbfa6",
+  barTextColor: "#ffffff",
+  colorSecondary: "#4cbfa6",
+  appContentBg: "#fff",
+  barSelectedColor: "#4cbfa6",
+  barBg: "black",
+  inputBg: "#ffffff",
+  inputBorder: "black",
+  inputTextColor: "black",
+  inputBorderRadius: 10,
+  brandImage:
+    "https://res.cloudinary.com/alvarosaburido/image/upload/v1616082729/as-portfolio/awesome-sushi-logo_aiaydr.png",
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import custom_theme from './custom_theme';
+
+addons.setConfig({
+  theme: custom_theme,
+});
