4cb534c89c8d7f3456df10533ac2312a4883dd7d
Bunghez Ovidiu <ovidiueduard@gmail.com>
2023-06-03 16:45:46 +0300

    feat(develop): tested on development

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['res.cloudinary.com'],
   },
-}
+  future: {
+    webpack5: true, // Enable Webpack 5
+  },
+  experimental: {
+    modern: true, // Enable modern script deployment strategy
+    polyfillsOptimization: true, // Optimize polyfills
+    asyncToPromises: true, // Enable async/await transformation
+    optimizeFonts: true, // Optimize font loading
+    optimizeImages: true, // Optimize image loading
+    optimizeCss: true, // Optimize CSS
+    scrollRestoration: true, // Enable scroll restoration
+  },
+};
 
-
-module.exports = nextConfig
+module.exports = nextConfig;
